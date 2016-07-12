05a4f876d17683c91cfcd7107317235a87c6d1c5
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-07-12 14:39:08 -0300

    Restart do estudo

--- contatooh/app/routes/contato.js
+++
@@ -1,6 +0,0 @@
-//var controller = require('../controllers/contato');
-module.exports = function(app){
-  var controller = app.controllers.contato;
-  app.get('/contatos', controller.listaContatos);
-  app.get('/contatos/:id', controller.obtemContato);
-};
--- contatooh/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var app = require('./config/express')();
-http.createServer(app).listen(app.get("porta"), function() {
-    //res.writeHead(200, {'Content-Type': 'text/plain'});
-    //res.end("Sou um servidor criado pelo Node");
-    console.log('Express server escutando na porta ' + app.get("porta"));
-});
