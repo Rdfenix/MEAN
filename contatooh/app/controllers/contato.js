c02764cf5dda594b00c812eb06d44d41090ebd45
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-07-15 14:08:53 -0300

    Estudo sobre M.E.A.N

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -1,8 +1,9 @@
 //var model = require('../models/contato');
 var contatos = [
-  {_id: 1, nome: "Rudnei Carlos de Oliveira"},
-  {_id: 2, nome: "Marcos Roberto de Oliveira"},
-  {_id: 3, nome: "Ana Paula de Oliveira"}
+  {_id: 1, nome: "Rudnei Carlos de Oliveira", email: "rudnei@email.com.br"},
+  {_id: 2, nome: "Marcos Roberto de Oliveira", email: "marcos@email.com.br"},
+  {_id: 3, nome: "Ana Paula de Oliveira", email: "ana.paula@email.com.br"},
+  {_id: 4, nome: "Roosevelt Celso de Oliveira", email: "roosevelt@email.com.br"}
 ];
 module.exports = function(){
   var controller = {};
@@ -18,5 +19,16 @@ module.exports = function(){
 
     contato ? res.json(contato) : res.status(404).send('Contato nao encontrado');
   };
+
+
+    controller.removerContato = function(req, res){
+      var idCOntato = req.params.id;
+      contatos = contatos.filter(function(contatos){
+          return contato._id != idCOntato;
+      });
+      res.status(204).end();
+      console.log("API: Remove Contato: " + idCOntato);
+    };
+
   return controller;
 };
--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,5 +1,50 @@
 var contato = angular.module('contatooh');
-contato.controller('ContatosController', function($scope){
+contato.controller('ContatosController', function($scope, $resource){
+
+  $scope.contatos = [];
+  //$scope.total = 0;
+  $scope.filtro = '';
+
+  var Contato = $resource('/contatos/:id');
+
+
+  function buscaContato(){
+    Contato.query(function(contatos){
+        $scope.contatos = contatos;
+      }, function(erro){
+          console.log("Não foi possivel obter a lista de contatos");
+          console.log(erro);
+      });
+    }
+
+    buscaContato();
+
+    $scope.remove = function(contato){
+      Contato.delete({id: contato._id});
+      console.log(contato);
+    }
+  //var promise = Contato.query().$promise;
+
+  /*promise
+    .then(function(contatos){
+      $scope.contatos = contatos;
+    })
+    .catch(function(erro){
+        console.log("Não foi possivel obter a lista de contatos");
+        console.log(erro);
+    });
+  $scope.incrementa = function(){
+    $scope.total++;
+  };*/
+
+  /*$http.get('/contatos')
+    .success(function(data){
+        $scope.contatos = data;
+    })
+    .error(function(statusText){
+        console.log("Não foi possivel obter a lista de contatos");
+        console.log(statusText);
+    });
   $scope.total  = 0;
   $scope.incrementa = function(){
     $scope.total++;
@@ -10,6 +55,6 @@ contato.controller('ContatosController', function($scope){
     {_id: 3, nome: "Ana Paula de Oliveira", email: "ana.paula@email.com.br"}
   ];
 
-  $scope.filtro = '';
+  $scope.filtro = '';*/
 
 });
